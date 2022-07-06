2f06d4fa449b01a67f0a82eadd73de179ab01095
jcagz96 <jcagz@iscte-iul.pt>
2022-07-06 23:28:32 +0100

    created MeetupDetail component

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,12 +1,13 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
 function MeetupDetails() {
   return (
-    <>
-      <img src="https://static.barcelo.com/content/dam/bhg/master/es/hoteles/republica-checa/praga/imagenes-praga/praga-rutas-1600.jpg.bhgimg.optm1100.jpg/1604687947914.jpg" alt="A first meetup" />
-
-      <h1>A First MeetUp</h1>
-      <address>Lisbon, 2612771</address>
-      <p>The meetup description</p>
-    </>
+    <MeetupDetail
+      title='A Second Meetup'
+      image='https://static.barcelo.com/content/dam/bhg/master/es/hoteles/republica-checa/praga/imagenes-praga/praga-rutas-1600.jpg.bhgimg.optm1100.jpg/1604687947914.jpg'
+      address='Some adress 5, 2232232 - lisbon'
+      description='this is a description 2'
+    />
   );
 }
 
