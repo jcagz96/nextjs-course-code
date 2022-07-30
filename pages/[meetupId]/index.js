4677f478eabe8807c3d3c22734db3417db387e44
jcagz96 <jcagz@iscte-iul.pt>
2022-07-30 23:05:54 +0100

    changed fallback to blocking

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -36,7 +36,7 @@ export async function getStaticPaths() {
 
 
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map(meetup => ({
       params: { meetupId: meetup._id.toString() }
     }))
